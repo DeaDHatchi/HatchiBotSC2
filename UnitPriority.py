d174e59df552056cf34583905eb9537cb178cd14
Travis Gerken <travis.g@cornerstonecredit.net>
2018-07-10 17:05:12 -0800

    Added UnitPriority to be worked on

---
+++ UnitPriority.py
@@ -0,0 +1,22 @@
+from sc2.constants import *
+
+
+unit_dict = {PROBE:       {'Ground': True, 'Air': False, 'AttackRange': 0.1},
+             ZEALOT:      {'Ground': True, 'Air': False, 'AttackRange': 0.1},
+             STALKER:     {'Ground': True, 'Air': True, 'AttackRange': 6},
+             ADEPT:       {'Ground': True, 'Air': False, 'AttackRange': 4},
+             SENTRY:      {'Ground': True, 'Air': True, 'AttackRange': 5},
+             HIGHTEMPLAR: {'Ground': True, 'Air': False, 'AttackRange': 6},
+             DARKTEMPLAR: {'Ground': True, 'Air': False, 'AttackRange': 0.1},
+             ARCHON:      {'Ground': True, 'Air': True, 'AttackRange': 3},
+             IMMORTAL:    {'Ground': True, 'Air': False, 'AttackRange': 6},
+             WARPPRISM:   {'Ground': False, 'Air': False, 'AttackRange': 0.1},
+             OBSERVER:    {'Ground': False, 'Air': False, 'AttackRange': 0.1},
+             COLOSSUS:    {'Ground': True, 'Air': False, 'AttackRange': 7},
+             DISRUPTOR:   {'Ground': True, 'Air': False, 'AttackRange': 7},
+             PHOENIX:     {'Ground': False, 'Air': True, 'AttackRange': 5},
+             VOIDRAY:     {'Ground': True, 'Air': True, 'AttackRange': 6},
+             ORACLE:      {'Ground': True, 'Air': False, 'AttackRange': 4},
+             CARRIER:     {'Ground': True, 'Air': True, 'AttackRange': 8},
+             TEMPEST:     {'Ground': True, 'Air': True, 'AttackRange': 10},
+             MOTHERSHIP:  {'Ground': True, 'Air': True, 'AttackRange': 7}}
